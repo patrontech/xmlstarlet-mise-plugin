dc3f749aa0bfa6793c191ee39d7bbb70c695c7f7
Gregory Higley <greg@prosumma.com>
2025-10-17 12:00:57 -0400

    Doing an ASDF plugin instead

--- hooks/available.lua
+++
@@ -1,7 +0,0 @@
--- hooks/available.lua
--- Returns a list of available versions for the tool
--- Documentation: https://mise.jdx.dev/tool-plugin-development.html#available-hook
-
-function PLUGIN:Available(ctx)
-  return { "1.6.1" }
-end
